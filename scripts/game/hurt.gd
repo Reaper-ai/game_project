5e372cbf03d141d616329974c4e05c15ae39ab0c
Gaurav <gauravupreti232+github@gmail.com>
2025-02-02 20:29:30 +0530

    changed hurt sfx

--- scripts/game/hurt.gd
+++ scripts/game/hurt.gd
@@ -1,5 +1,5 @@
 extends AudioStreamPlayer2D
-const fx = preload("res://assets/sounds/hurt.wav")
+const fx = preload("res://assets/sounds/hurt.mp3")
 func _play_music(music,volume = 0.0):
 	stream = music
 	volume_db = volume
