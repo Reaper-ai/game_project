4c0ea694cf212c214c680522f55e2dd9e75b6753
Gaurav <gauravupreti232+github@gmail.com>
2025-02-02 21:05:24 +0530

    test run

--- scripts/game/global.gd
+++ scripts/game/global.gd
@@ -4,6 +4,6 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 var VERTICAL = -270.0
 var SPEED = 120.0
-var coins = 0
+var coins =0
 var last_coin_x = 0
 var last_coin_y = 0
