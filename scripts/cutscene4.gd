ad2ee55e1313ae6fd7bf94e3824b5768dd5439ed
Gaurav <gauravupreti232+github@gmail.com>
2025-02-02 11:00:45 +0530

    cutscene zero  added

--- scripts/cutscene4.gd
+++ scripts/cutscene4.gd
@@ -1,8 +1,8 @@
 extends Node
 signal cutscene4_over
 
-var txt = "[color=red]22,122 hrs since the crash[/color]\n\n Axton finally managed to escape the unknown planet. \n[color=green][font_size=80]\nTHE END[/font_size][/color]"
-var credits = "[center]\nGaurav Upreti\n\nJuss Patel\n\nLalrautmawia Hamar\n\n[/center]"
+var txt = "[color=red]24,122 hrs since the crash[/color]\n\n Axton finally managed to escape the unknown planet. \n[color=green][font_size=80]\nTHE END[/font_size][/color]"
+var credits = "[center][font_size=50]\nGaurav Upreti\n\nJuss Patel\n\nLalruatmawia Hmar\n\n[/font_size][/center]"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -12,13 +12,13 @@ func _ready():
 	await get_tree().create_timer(1).timeout
 	$AnimationPlayer.play("text")
 	await get_tree().create_timer(10).timeout
-	emit_signal("cutscene4_over")
+	
 
 	#scene2
-	
-	#scene3
 	$RichTextLabel.visible_ratio = 0
 	$RichTextLabel.text = credits 
 	await get_tree().create_timer(1).timeout
 	$AnimationPlayer.play("text")
 	await get_tree().create_timer(10).timeout
+	
+	emit_signal("cutscene4_over")
---
+++ scripts/cutscene_0.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+
+signal cutscene0_over
+
+var txt = "[color=red]Brokenness[/color] is not the [color=red]end[/color], but a [color=aqua]new beginning.[/color]\n\n\n– a [color=aqua]chance[/color] to [color=green]rebuild[/color], to [color=green]restart[/color], and to [color=green]rediscover.[/color]" 
+var disclaimer = "[center][color=orange][b]Disclaimer :[/b] \n seizure warning! player discreation is advised.[/color][/center]"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$RichTextLabel.text = "[center]" + txt + "[/center]"
+	fade_in()
+	await get_tree().create_timer(2).timeout
+	emit_signal("cutscene0_over")
+	$Disclaimer.visible_ratio = 0
+	$Disclaimer.text = disclaimer
+	await get_tree().create_timer(1).timeout
+	$AnimationPlayer.play("text")
+	await get_tree().create_timer(5).timeout
+func fade_in():
+	# Create a Tween
+	$RichTextLabel.modulate.a = 0 
+	await get_tree().create_timer(1).timeout
+	var tween = get_tree().create_tween()
+	# Animate the opacity from 0 to 1 over 2 seconds
+	tween.tween_property($RichTextLabel, "modulate:a", 1, 5).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT) 
